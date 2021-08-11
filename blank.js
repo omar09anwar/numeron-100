aac3fa7e018fde07b0b054808e76811b7b469a0b
omar09anwar <76914816+omar09anwar@users.noreply.github.com>
2021-08-11 12:28:47 +0400

    Add files via upload

---
+++ blank.js
@@ -0,0 +1,11 @@
+const no = document.getElementById("Quit")
+
+no.onclick = () => {
+location.href = "gameover.html"
+}
+
+const yes = document.getElementById("continue")
+
+yes.onclick = () => {
+location.href = "./game.html"
+}
